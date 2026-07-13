dd7ae21aaca88ff0d403b6c9a094550f9cd69aae
Nicolas Lazaro <125311555+CooperBigFoot@users.noreply.github.com>
2026-07-13 16:40:52 +0200

    chore(release): reset version to 0.1.0 (#7)

--- src/hcx/__init__.py
+++ src/hcx/__init__.py
@@ -1,4 +1,4 @@
-__version__ = "0.1.8"
+__version__ = "0.1.0"
 
 from hcx.batch import Batch, BatchMetadata, GriddedDynamic, GriddedStatic
 from hcx.conformance import ConformanceCheck, ConformanceError, assert_conforms, check_conformance
